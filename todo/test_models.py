19982d8ad997aeb8dc2e7d709160f86326a1d524
yannickferenczi <>
2023-06-05 16:55:15 +0000

    Add testing for models

--- todo/test_models.py
+++ todo/test_models.py
@@ -1,9 +1,14 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Item
 
 
-class TestDjango(TestCase):
+class TestModels(TestCase):
 
-    def test_this_thing_works(self):
-        self.assertEqual(1, 1)
+    def test_done_default_to_false(self):
+        item = Item.objects.create(name="Test Todo Item")
+        self.assertFalse(item.done)
+
+    def test_item_string_method_returns_name(self):
+        item = Item.objects.create(name="Test Todo Item")
+        self.assertEqual(str(item), "Test Todo Item")
