13fd2a6420e36c6a75a0600af736a1f4cf0eb0ee
Yannick Ferenczi <yannick.ferenczi@outlook.com>
2023-06-06 19:36:13 +0000

    Add development as environment variable

--- to_do_walkthrough/settings.py
+++ to_do_walkthrough/settings.py
@@ -17,6 +17,8 @@ import dj_database_url
 if os.path.exists("env.py"):
     import env
 
+development = os.environ.get("DEVELOPMENT", False)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -28,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = development
 
 ALLOWED_HOSTS = ["8000-yannickferenczi-to-do-wa-8gfepcvh1w.us2.codeanyapp.com"]
 
@@ -79,16 +81,17 @@ WSGI_APPLICATION = 'to_do_walkthrough.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-DATABASES = {
-    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
-}
+if development:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    DATABASES = {
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
