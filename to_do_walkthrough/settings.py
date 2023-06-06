8788a246b1a90904852a37856891d11fe7c8919a
Yannick Ferenczi <yannick.ferenczi@outlook.com>
2023-06-06 18:51:43 +0000

    Try to fix the 405 error

--- to_do_walkthrough/settings.py
+++ to_do_walkthrough/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["8000-yannickferenczi-to-do-wa-8gfepcvh1w.us2.codeanyapp.com"]
 
 
 # Application definition
