8eeb9afee50366beada69f766cae3cfe1f9a83fd
Yannick Ferenczi <yannick.ferenczi@outlook.com>
2023-06-06 13:06:47 +0000

    Fix another issue

--- to_do_walkthrough/settings.py
+++ to_do_walkthrough/settings.py
@@ -28,10 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [
-    '8000-yannickferenczi-to-do-wa-8gfepcvh1w.us2.codeanyapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
